5d1673469dc8c6a8a2f0b40402fbbe463deab56d
RafPe <rafal@pieniazek.nl>
2021-03-16 23:51:37 +0100

    adds http client (go-resty)

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/RafPe/jitsi-jvb-prometheus-adapter
 
 go 1.15
 
-require github.com/prometheus/client_golang v1.9.0
+require (
+	github.com/go-resty/resty/v2 v2.5.0
+	github.com/prometheus/client_golang v1.9.0
+)
--- main.go
+++ main.go
@@ -1,11 +1,50 @@
 package main
 
 import (
-	"github.com/prometheus/client_golang/prometheus"
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/go-resty/resty/v2"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
 func main() {
+
+	// Create a Resty Client
+	client := resty.New()
+
+	resp, err := client.R().
+		EnableTrace().
+		Get("https://google.com")
+
+	// Explore response object
+	fmt.Println("Response Info:")
+	fmt.Println("  Error      :", err)
+	fmt.Println("  Status Code:", resp.StatusCode())
+	fmt.Println("  Status     :", resp.Status())
+	fmt.Println("  Proto      :", resp.Proto())
+	fmt.Println("  Time       :", resp.Time())
+	fmt.Println("  Received At:", resp.ReceivedAt())
+	fmt.Println("  Body       :\n", resp)
+	fmt.Println()
+
+	// Explore trace info
+	fmt.Println("Request Trace Info:")
+	ti := resp.Request.TraceInfo()
+	fmt.Println("  DNSLookup     :", ti.DNSLookup)
+	fmt.Println("  ConnTime      :", ti.ConnTime)
+	fmt.Println("  TCPConnTime   :", ti.TCPConnTime)
+	fmt.Println("  TLSHandshake  :", ti.TLSHandshake)
+	fmt.Println("  ServerTime    :", ti.ServerTime)
+	fmt.Println("  ResponseTime  :", ti.ResponseTime)
+	fmt.Println("  TotalTime     :", ti.TotalTime)
+	fmt.Println("  IsConnReused  :", ti.IsConnReused)
+	fmt.Println("  IsConnWasIdle :", ti.IsConnWasIdle)
+	fmt.Println("  ConnIdleTime  :", ti.ConnIdleTime)
+	fmt.Println("  RequestAttempt:", ti.RequestAttempt)
+	fmt.Println("  RemoteAddr    :", ti.RemoteAddr.String())
+
 	http.Handle("/metrics", promhttp.Handler())
 	log.Fatal(http.ListenAndServe(":9101", nil))
 }

7e40a6f804d48bff8884ad609d9709fb707c47be
RafPe <rafal@pieniazek.nl>
2021-03-17 00:11:25 +0100

    adds interrupt handling

--- main.go
+++ main.go
@@ -1,15 +1,33 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"github.com/go-resty/resty/v2"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
+var (
+	srv = &http.Server{
+		Addr: fmt.Sprintf("127.0.0.1:%d", 9001),
+	}
+	interrupt = make(chan os.Signal, 1) // Handle the interrupts with GO routines
+
+)
+
+func init() {
+	signal.Notify(interrupt, os.Interrupt, syscall.SIGTERM)
+}
+
 func main() {
+	http.Handle("/metrics", promhttp.Handler())
 
 	// Create a Resty Client
 	client := resty.New()
@@ -45,6 +63,17 @@ func main() {
 	fmt.Println("  RequestAttempt:", ti.RequestAttempt)
 	fmt.Println("  RemoteAddr    :", ti.RemoteAddr.String())
 
-	http.Handle("/metrics", promhttp.Handler())
-	log.Fatal(http.ListenAndServe(":9101", nil))
+	go func() {
+		for {
+			select {
+			case s := <-interrupt:
+				log.Printf("Signal (%d) received, stopping", s)
+				time.Sleep(time.Duration(2 * time.Second)) // wait specific amount seconds to close all requests ...
+
+				srv.Shutdown(context.Background())
+			}
+		}
+	}()
+
+	log.Fatal(srv.ListenAndServe(), nil)
 }

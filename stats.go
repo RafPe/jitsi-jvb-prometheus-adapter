232a1537fabaaa7adb228ae9f0cc4ecb827e753c
RafPe <rafal@pieniazek.nl>
2021-03-24 10:35:11 +0100

    update stats with correct types for float

--- stats.go
+++ stats.go
@@ -9,8 +9,8 @@ type JVBStatistics struct {
 	MucClientsConnected                   int     `json:"muc_clients_connected" prom:"Gauge;Number of configured MUC clients that are connected to XMPP."`
 	TotalParticipants                     int     `json:"total_participants" prom:"Gauge;The current number of participants."`
 	TotalPacketsReceived                  int     `json:"total_packets_received" prom:"Counter;Total number of packets received"`
-	RttAggregate                          int     `json:"rtt_aggregate" prom:"Gauge;An average value (in milliseconds) of the RTT across all streams."`
-	PacketRateUpload                      int     `json:"packet_rate_upload" prom:"Gauge;The total outgoing packet rate for the video bridge in packets per second."`
+	RttAggregate                          float64 `json:"rtt_aggregate" prom:"Gauge;An average value (in milliseconds) of the RTT across all streams."`
+	PacketRateUpload                      float64 `json:"packet_rate_upload" prom:"Gauge;The total outgoing packet rate for the video bridge in packets per second."`
 	P2PConferences                        int     `json:"p2p_conferences" prom:"Gauge;The current number of p2p conferences."`
 	TotalLossLimitedParticipantSeconds    int     `json:"total_loss_limited_participant_seconds" prom:"Counter;The total number of participant-seconds that are loss-limited."`
 	OctoSendBitrate                       int     `json:"octo_send_bitrate" prom:"Gauge;The total outgoing bitrate for the OCTO video bridge in kilobits per second."`
@@ -30,15 +30,15 @@ type JVBStatistics struct {
 	OctoConferences                       int     `json:"octo_conferences" prom:"Gauge;The current number of OCTO conferences."`
 	NumEpsNoMsgTransportAfterDelay        int     `json:"num_eps_no_msg_transport_after_delay"`
 	EndpointsSendingVideo                 int     `json:"endpoints_sending_video" prom:"Gauge;Endpoint sending video"`
-	PacketRateDownload                    int     `json:"packet_rate_download" prom:"Gauge;The total incoming packet rate for the video bridge in packets per second."`
+	PacketRateDownload                    float64 `json:"packet_rate_download" prom:"Gauge;The total incoming packet rate for the video bridge in packets per second."`
 	MucClientsConfigured                  int     `json:"muc_clients_configured" prom:"Gauge;Number of configured MUC clients."`
-	OutgoingLoss                          int     `json:"outgoing_loss" prom:"Gauge;The fraction of lost outgoing RTP packets. This is based on incoming RTCP Receiver Reports, and an attempt to subtract the fraction of packets that were not sent (i.e. were lost before they reached the bridge). Further, this is averaged over all streams of all users as opposed to all packets, so it is not correctly weighted. This is not accurate, but may be a useful metric nonetheless."`
-	OverallLoss                           int     `json:"overall_loss"`
-	ConferenceSizes                       []int   `json:"conference_sizes"` //TODO: Work on edge case scenarios
+	OutgoingLoss                          float64 `json:"outgoing_loss" prom:"Gauge;The fraction of lost outgoing RTP packets. This is based on incoming RTCP Receiver Reports, and an attempt to subtract the fraction of packets that were not sent (i.e. were lost before they reached the bridge). Further, this is averaged over all streams of all users as opposed to all packets, so it is not correctly weighted. This is not accurate, but may be a useful metric nonetheless."`
+	OverallLoss                           float64 `json:"overall_loss"`
+	ConferenceSizes                       []int   `json:"conference_sizes" prom:"Untyped;Histogram of conference sizes"`
 	TotalPacketsSentOcto                  int     `json:"total_packets_sent_octo" prom:"Gauge;The total of sent dropped packets handled by the OCTO video bridge."`
-	ConferencesByVideoSenders             []int   `json:"conferences_by_video_senders"`
-	StressLevel                           int     `json:"stress_level" prom:"Gauge;Stress Level reported to Jicofo by the videobridge."`
-	JitterAggregate                       int     `json:"jitter_aggregate" prom:"Gauge;Experimental. An average value (in milliseconds) of the jitter calculated for incoming and outgoing streams. This hasn't been tested and it is currently not known whether the values are correct or not."`
+	ConferencesByVideoSenders             []int   `json:"conferences_by_video_senders" prom:"Untyped;Histogram of conference by video senders"`
+	StressLevel                           float64 `json:"stress_level" prom:"Gauge;Stress Level reported to Jicofo by the videobridge."`
+	JitterAggregate                       float64 `json:"jitter_aggregate" prom:"Gauge;Experimental. An average value (in milliseconds) of the jitter calculated for incoming and outgoing streams. This hasn't been tested and it is currently not known whether the values are correct or not."`
 	TotalIceSucceededTCP                  int     `json:"total_ice_succeeded_tcp" prom:"Counter; Total number of times ICE succeeded over TCP."`
 	OctoEndpoints                         int     `json:"octo_endpoints" prom:"Gauge;The current number of OCTO endpoints."`
 	CurrentTimestamp                      string  `json:"current_timestamp"`
@@ -46,12 +46,12 @@ type JVBStatistics struct {
 	Conferences                           int     `json:"conferences" prom:"Gauge;The current number of conferences."`
 	Participants                          int     `json:"participants" prom:"Gauge;The current number of participants."`
 	LargestConference                     int     `json:"largest_conference" prom:"Gauge;The number of participants in the largest conference currently hosted on the bridge."`
-	TotalPacketsSent                      int     `json:"total_packets_sent"`
+	TotalPacketsSent                      int     `json:"total_packets_sent" prom:"Gauge;The total of sent packets handled by video bridge."`
 	TotalDataChannelMessagesSent          int     `json:"total_data_channel_messages_sent" prom:"Counter;The total number messages sent through data channels."`
-	IncomingLoss                          int     `json:"incoming_loss" prom:"Gauge;The fraction of lost incoming RTP packets. This is based on RTP sequence numbers and is relatively accurate."`
+	IncomingLoss                          float64 `json:"incoming_loss" prom:"Gauge;The fraction of lost incoming RTP packets. This is based on RTP sequence numbers and is relatively accurate."`
 	TotalBytesReceivedOcto                int     `json:"total_bytes_received_octo" prom:"Gauge;The total incoming bit rate for the OCTO video bridge in bytes per second."`
 	OctoSendPacketRate                    int     `json:"octo_send_packet_rate" prom:"Gauge;The total outgoing packet rate for the OCTO video bridge in packets per second."`
-	ConferencesByAudioSenders             []int   `json:"conferences_by_audio_senders"`
+	ConferencesByAudioSenders             []int   `json:"conferences_by_audio_senders" prom:"Untyped;Histogram of conference by audio senders"`
 	TotalConferencesCreated               int     `json:"total_conferences_created" prom:"Counter;The total number of conferences created on the bridge."`
 	TotalIceFailed                        int     `json:"total_ice_failed" prom:"Counter;total number of times ICE failed."`
 	Threads                               int     `json:"threads" prom:"Gauge;The number of Java threads that the video bridge is using."`
